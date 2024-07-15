191fcd915a4fddc314fdbb196f1d0f3c95209d11
MINEL <116800147+Kujundzic-Minel@users.noreply.github.com>
2024-07-15 22:25:14 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+	import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+			<RouterLink to="/events">Agenda</RouterLink>
+      <RouterLink to="/contact">Contact</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Page d'Accueil</h1>
+</template>
