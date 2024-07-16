cf308916916ced4d99b557f68a09deff1accfbd4
MINEL <116800147+Kujundzic-Minel@users.noreply.github.com>
2024-07-16 20:37:42 +0200

    start header

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import Header from './components/Header.vue';
 </script>
 
 <template>
-  <Header />
-  <RouterView />
+    <Header />
+    <Suspense>
+    <RouterView />
+    </Suspense>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,12 @@
 <script setup lang="ts">
 </script>
 <template>
-    <nav class="bg-red">
-        <RouterLink to="/">Accueil</RouterLink>
-		<RouterLink to="/agents">Agents</RouterLink>
-        <RouterLink to="/armes">Armes</RouterLink>
-        <RouterLink to="/maps">Maps</RouterLink>
+    <nav class="bg-[#111111] p-8 font-tungsten text-[#ededed]">
+        <div class="px-8 space-x-8 text-center text-3xl">
+            <RouterLink to="/">Accueil</RouterLink>
+            <RouterLink to="/agents">Agents</RouterLink>
+            <RouterLink to="/armes">Armes</RouterLink>
+            <RouterLink to="/maps">Maps</RouterLink>
+        </div>
     </nav>
 </template>
